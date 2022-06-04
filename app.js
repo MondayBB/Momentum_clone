6452be14a8d8f58bf0d119a0a21b6670b5578469
yejin <jynhpss@naver.com>
2022-06-05 00:52:32 +0900

    login 정보 저장

---
+++ app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+//일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용한다.
+const HIDDEN_CLASS = "hidden";  
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+  const userName = loginInput.value;
+  
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASS);
+  localStorage.setItem(USERNAME_KEY, userName);
+  
+  paintGreetings(userName);
+}
+
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null){
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else{ 
+  paintGreetings(savedUserName);
+}
